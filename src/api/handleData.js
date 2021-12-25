4500bc79a956d0d0b8215366aea6411de724d65a
tranvinhhung <tranvinhung2k@gmail.com>
2021-12-25 17:59:15 +0700

    handleData From axios cleaner

--- src/api/handleData.js
+++ src/api/handleData.js
@@ -1,3 +1,8 @@
+import {
+  getGender,
+  ramdomValueArrayGender,
+  listAnimeWithGender,
+} from "./index";
 export const handerData = (data) => {
   let array = data.map((el) => {
     let relaship = el.relationships.map((el) =>
@@ -10,4 +15,14 @@ export const handerData = (data) => {
     };
   });
   return array;
+};
+export const handerAnime = async () => {
+  let arrayList = [];
+  let reponseAnimeList = await getGender();
+  let ramdomGender = await ramdomValueArrayGender(reponseAnimeList, 3);
+  for (let i = 0; i < ramdomGender.length; i++) {
+    let ray = await listAnimeWithGender(ramdomGender[i]);
+    arrayList.push({ ...ray, gender: ramdomGender[i] });
+  }
+  return arrayList;
 };
