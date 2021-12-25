f022e46a831db0e2e3de324632e08b746e78199f
tranvinhhung <tranvinhung2k@gmail.com>
2021-12-25 17:57:31 +0700

    add css and Handle logic

--- src/components/AnimePlay/AnimePlay.js
+++ src/components/AnimePlay/AnimePlay.js
@@ -6,6 +6,8 @@ import Typography from "@mui/material/Typography";
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { handleActiveClose, handleActiveOpen } from "./../../reduces/index";
+import PlayCircleRoundedIcon from "@mui/icons-material/PlayCircleRounded";
+import PauseCircleRoundedIcon from "@mui/icons-material/PauseCircleRounded";
 import {
   useNavigate,
   NavLink,
@@ -15,12 +17,13 @@ import {
 } from "react-router-dom";
 import { handleAsync } from "../Error/Error";
 // Import Swiper styles
-import { getAnimeWidthId } from "./../../api/index";
+import { getAnimeWidthId, songWidthId } from "./../../api/index";
 import "./anime.scss";
 
 function AnimePlay(props) {
   const [anime, setAnime] = useState(null);
-
+  const [song, setSong] = useState(null);
+  const [play, setPlay] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const activeDialog = useSelector((state) => state.mycounter.active);
@@ -50,18 +53,45 @@ function AnimePlay(props) {
     navigate(-1);
     dispatch(handleActiveClose());
   };
+  const handlePlay = () => {
+    let emm = document.querySelector("#emm");
+    if (play) {
+      emm.pause();
+      setPlay(false);
+    }
+    if (!play) {
+      emm.play();
+      setPlay(true);
+    }
+    // console.log(emm);
+  };
+  const handleStop = () => {
+    let emm = document.querySelector("#emm");
+    // console.log(emm);
+    emm.pause();
+  };
 
   const { id } = useParams();
   //console.log(id);
   useEffect(() => {
     handleAsync(async () => {
-      const animeId = await getAnimeWidthId(id);
+      // const animeId = await getAnimeWidthId(id);
+      // const animeSong = await songWidthId(id);
+      const handleAll = await Promise.all([
+        getAnimeWidthId(id),
+        songWidthId(id),
+      ]);
 
-      const anime = animeId.data.data;
+      console.log(handleAll);
+      const [animethis, songthis] = handleAll;
 
-      setAnime(anime);
+      const animedata = animethis.data.data;
+      const songdata = songthis.data.data;
+      setSong(songdata);
+      setAnime(animedata);
     })();
   }, []);
+
   return (
     <section className="mainContainer anime">
       <figure
@@ -90,7 +120,35 @@ function AnimePlay(props) {
         </Button>
         <img src={`${anime?.cover_image}`} alt={`${anime?.titles.en}`} />
       </figure>
-      <div className="anime_descriptions">{`${anime?.descriptions.en}`}</div>
+      <div className="anime_descriptions">
+        {`${anime?.descriptions.en}`}
+        {/* {song.includes("does not exists") && <div>Không có nhạc sorry!!</div>} */}
+        {song?.["preview_url"] && (
+          <div className="songAudio">
+            <audio
+              id="emm"
+              src={song?.["preview_url"]}
+              align="baseline"
+              border="0"
+              width={300}
+              height="200"
+              autostart="false"
+              loop={true}
+              loading="lazy"
+            ></audio>
+            {!play && (
+              <PlayCircleRoundedIcon
+                onClick={handlePlay}
+              ></PlayCircleRoundedIcon>
+            )}
+            {play && (
+              <PauseCircleRoundedIcon
+                onClick={handlePlay}
+              ></PauseCircleRoundedIcon>
+            )}
+          </div>
+        )}
+      </div>
       {/*pôpver */}
 
       {/* dialog */}
