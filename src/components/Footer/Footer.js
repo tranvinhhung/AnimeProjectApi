88a47eff6223ada1ce6480829b9e2daf087fb95f
tranvinhhung <tranvinhung2k@gmail.com>
2021-12-27 18:34:13 +0700

    create new footer and add new ui footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,33 @@
 import React from "react";
-
+import "./footer.scss";
 function Footer() {
-  return <div className="container">Footer</div>;
+  return (
+    <footer className="container footer">
+      <h1>MyAnime</h1>
+      <ul>
+        <li>Get started</li>
+        <li>Home</li>
+        <li>Sign up</li>
+        <li>Dowloads</li>
+      </ul>
+      <ul>
+        <li>About us</li>
+        <li>Company</li>
+        <li>Imformation</li>
+      </ul>
+      <ul>
+        <li>Support</li>
+        <li>FAQ</li>
+        <li>Help desk</li>
+      </ul>
+      <ul className="icon">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </footer>
+  );
 }
 
 export default Footer;
