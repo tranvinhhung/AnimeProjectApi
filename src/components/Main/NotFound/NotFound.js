24bd87d1825942610812dc7c3e4c1f9cfebb9116
tranvinhhung <tranvinhung2k@gmail.com>
2021-12-31 17:28:31 +0700

    fix ui Notfound

--- src/components/Main/NotFound/NotFound.js
+++ src/components/Main/NotFound/NotFound.js
@@ -1,7 +1,23 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import "./notfound.scss";
 function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div id="oopss">
+      <div id="error-text">
+        <img
+          src="https://cdn.rawgit.com/ahmedhosna95/upload/1731955f/sad404.svg"
+          alt="404"
+        />
+        <span>404 PAGE</span>
+        <p class="p-a">. The page you were looking for could not be found</p>
+
+        <Link to="/home" class="back">
+          ... Back to home
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default NotFound;
