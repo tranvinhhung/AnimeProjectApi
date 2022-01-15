c0f73dca25303edb5dc243cbef65fd810f352046
tranvinhhung <tranvinhung2k@gmail.com>
2022-01-15 11:30:46 +0700

    add view for list Love animes

--- src/components/AnimeComponents/AnimeListLove/AnimeListLove.js
+++ src/components/AnimeComponents/AnimeListLove/AnimeListLove.js
@@ -2,35 +2,46 @@ import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import jwt_decode from "jwt-decode";
 import { hanleListAnimeWithArrayId } from "./../../../api/index";
+import Card from "./../../Card/Card";
 const AnimeListLove = () => {
   const listLoveAnimeId = useSelector((state) => state.myUsers.users);
-  const [animeLoveList, setanimeLoveList] = useState();
-  // useEffect(() => {
-  //   (async () => {
-  //     try {
-  //       const token = localStorage.getItem("token");
-  //       if (!token) return;
-  //       if (token) {
-  //         let decode = jwt_decode(token);
-  //         let idUser = decode.id;
+  const [animeLoveList, setanimeLoveList] = useState([]);
+  useEffect(() => {
+    (async () => {
+      try {
+        const token = localStorage.getItem("token");
+        if (!token) return;
+        if (token) {
+          let decode = jwt_decode(token);
+          let idUser = decode.id;
 
-  //         let animeListLoveFilterWithId = listLoveAnimeId.filter(
-  //           (el) => el.user._id === idUser
-  //         );
-  //         console.log(animeListLoveFilterWithId);
-  //         let listId = animeListLoveFilterWithId[0]?.listLove
-  //           ?.filter((el) => (el.id ? true : false))
-  //           .map((el) => el.id);
-  //         console.log(listId);
-  //         let myDataListLove = await hanleListAnimeWithArrayId(listId);
-  //         console.log(myDataListLove);
-  //       }
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   })();
-  // }, []);
-  return <div></div>;
+          let animeListLoveFilterWithId = listLoveAnimeId.filter(
+            (el) => el.user._id === idUser
+          );
+          console.log(animeListLoveFilterWithId);
+          let listId = animeListLoveFilterWithId[0]?.listLove
+            ?.filter((el) => (el.id ? true : false))
+            .map((el) => el.id);
+          console.log(listId);
+          let myDataListLove = await hanleListAnimeWithArrayId(listId);
+          console.log(myDataListLove);
+          setanimeLoveList(myDataListLove);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, [listLoveAnimeId]);
+  return (
+    <div className="mainContainer genDerList">
+      <div style={{ padding: "1rem", fontSize: 30 }}>List animes my Love</div>
+      <div className="containerListGenderImg">
+        {animeLoveList.map((el, index) => (
+          <Card key={index} data={el.data.data} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AnimeListLove;
