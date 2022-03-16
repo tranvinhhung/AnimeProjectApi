c8e1ced6f346be4b0146a7ac0a24ae3309cee1ac
Tran Vinh Hung <hungtv@MECORP.LOCAL>
2022-03-16 09:54:01 +0700

    test2

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Admin() {
   // console.log("testbarnádas");
-  console.log("testttttttt");
+  console.log("testt");
   // console.log("123123123213");
   console.log("maianana");
   return <div>Admin33333</div>;
