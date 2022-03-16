a1654bdd006ae754f85e0f6cb12bfc3bb0024028
Tran Vinh Hung <hungtv@MECORP.LOCAL>
2022-03-16 09:48:59 +0700

    asda

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Admin() {
-  console.log("testbarnádas");
+  // console.log("testbarnádas");
   console.log("testttttttt");
   // console.log("123123123213");
   return <div>Admin33333</div>;
