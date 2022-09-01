1a1cf1724bb384c1347f0e113e5b0f1e2c07c9f2
hungtv <hungtv@payme.vn>
2022-09-01 13:17:12 +0700

    fix reset

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 function Admin() {
-  console.log("testbarnádas");
-  console.log("testttttttt");
+  // console.log("testbarnádas");
+  console.log("testt");
   // console.log("123123123213");
+  console.log("maianana");
   return <div>Admin33333</div>;
 }
 
